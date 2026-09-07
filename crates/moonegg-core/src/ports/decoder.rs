34eec508374cc9366da5f38c54742c9731cb6015
dust-zed <1186669164@qq.com>
2026-09-07 19:58:26 +0800

    chore: Decoder简单的定义

--- crates/moonegg-core/src/ports/decoder.rs
+++ crates/moonegg-core/src/ports/decoder.rs
@@ -1,1 +1,38 @@
+use crate::media::{DecodedFrame, Packet};
 
+#[derive(Debug)]
+pub enum DecodeInput {
+    Packet(Packet),
+    EndOfStream,
+}
+
+#[derive(Debug)]
+pub enum SubmitResult {
+    Accepted,
+    Backpressure(DecodeInput),
+}
+
+#[derive(Debug)]
+pub enum ReceiveResult<T> {
+    Frame(DecodedFrame<T>),
+    NeedInput,
+    EndOfStream,
+}
+
+#[derive(Debug)]
+pub enum DecodeError {
+    InvalidData,
+    InvalidState,
+    Unsupported,
+    Platform,
+}
+
+pub trait Decoder {
+    type Output;
+
+    fn submit(&mut self, input: DecodeInput) -> Result<SubmitResult, DecodeError>;
+
+    fn receive(&mut self) -> Result<ReceiveResult<Self::Output>, DecodeError>;
+
+    fn flush(&mut self) -> Result<(), DecodeError>;
+}
